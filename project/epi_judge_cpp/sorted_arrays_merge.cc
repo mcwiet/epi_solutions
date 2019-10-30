acbb830f31e9ca07539c64e8ee503608d46b570f
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-10-30 12:58:49 -0400

    Committing brute force approach to 10.1.

--- project/epi_judge_cpp/sorted_arrays_merge.cc
+++ project/epi_judge_cpp/sorted_arrays_merge.cc
@@ -1,15 +1,34 @@
 #include <vector>
+#include <queue>
+#include <functional>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::priority_queue;
 vector<int> MergeSortedArrays(const vector<vector<int>>& sorted_arrays) {
-  // TODO - you fill in here.
-  return {};
+	int vals = 0;
+	for (auto& vec : sorted_arrays) {
+		for (auto& num : vec)
+			++vals;
+	}
+
+	priority_queue<int, vector<int>, std::greater<int>> nums(0, vals);
+	for (auto& vec : sorted_arrays) {
+		for (auto& num : vec)
+			nums.emplace(num);
+	}
+	vector<int> ret;
+	while (nums.size()) {
+		ret.push_back(nums.top());
+		nums.pop();
+	}
+
+	return ret;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"sorted_arrays"};
-  return GenericTestMain(args, "sorted_arrays_merge.cc",
-                         "sorted_arrays_merge.tsv", &MergeSortedArrays,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "sorted_arrays" };
+	return GenericTestMain(args, "sorted_arrays_merge.cc",
+		"sorted_arrays_merge.tsv", &MergeSortedArrays,
+		DefaultComparator{}, param_names);
 }
