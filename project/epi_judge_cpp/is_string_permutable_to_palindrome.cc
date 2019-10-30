b9d56a62a0131206bc7db51a42e1a916b2a40bad
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-10-30 14:25:59 -0400

    Adding 12.1 solution.

--- project/epi_judge_cpp/is_string_permutable_to_palindrome.cc
+++ project/epi_judge_cpp/is_string_permutable_to_palindrome.cc
@@ -1,16 +1,26 @@
 #include <string>
+#include <unordered_set>
 #include "test_framework/generic_test.h"
 using std::string;
-
+using std::unordered_set;
 bool CanFormPalindrome(const string& s) {
-  // TODO - you fill in here.
-  return true;
+	unordered_set<char> chars;
+
+	for (auto& c : s) {
+		auto it = chars.find(c);
+		if (it == chars.end())
+			chars.insert(c);
+		else
+			chars.erase(it);
+	}
+
+	return chars.size() <= 1;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"s"};
-  return GenericTestMain(args, "is_string_permutable_to_palindrome.cc",
-                         "is_string_permutable_to_palindrome.tsv",
-                         &CanFormPalindrome, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "s" };
+	return GenericTestMain(args, "is_string_permutable_to_palindrome.cc",
+		"is_string_permutable_to_palindrome.tsv",
+		&CanFormPalindrome, DefaultComparator{}, param_names);
 }
