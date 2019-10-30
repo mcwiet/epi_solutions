51790cd16e3176cc7d17df59e496bb2b1fbd2340
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-10-30 13:54:47 -0400

    Solution for 11.1.

--- project/epi_judge_cpp/search_first_key.cc
+++ project/epi_judge_cpp/search_first_key.cc
@@ -2,14 +2,30 @@
 #include "test_framework/generic_test.h"
 using std::vector;
 
-int SearchFirstOfK(const vector<int>& A, int k) {
-  // TODO - you fill in here.
-  return 0;
+int SearchFirstOfK(const vector<int>& a, int k) {
+	int u = a.size() - 1;
+	int l = 0;
+	int i = -1;
+
+	// Initial BST
+	while (l <= u) {
+		int m = l + (u - l) / 2;
+		if (k < a[m])
+			u = m - 1;
+		else if (k > a[m])
+			l = m + 1;
+		else {
+			u = m - 1;
+			i = m;
+		}
+	}
+
+	return i;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "k"};
-  return GenericTestMain(args, "search_first_key.cc", "search_first_key.tsv",
-                         &SearchFirstOfK, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "A", "k" };
+	return GenericTestMain(args, "search_first_key.cc", "search_first_key.tsv",
+		&SearchFirstOfK, DefaultComparator{}, param_names);
 }
