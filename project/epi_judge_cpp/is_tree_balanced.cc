1a5b11612e830b6c62f0a0b71f5219e3dacb738d
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-10-30 12:14:24 -0400

    Adding solution for problem 9.1

--- project/epi_judge_cpp/is_tree_balanced.cc
+++ project/epi_judge_cpp/is_tree_balanced.cc
@@ -1,13 +1,22 @@
 #include "binary_tree_node.h"
 #include "test_framework/generic_test.h"
+
 bool IsBalanced(const unique_ptr<BinaryTreeNode<int>>& tree) {
-  // TODO - you fill in here.
-  return true;
+	if (!tree)
+		return true;
+	if ((!tree->left) && (!tree->right))
+		return true;
+	else if ((!tree->left) && ((tree->right->left) || (tree->right->right)))
+		return false;
+	else if ((!tree->right) && ((tree->left->left) || (tree->left->right)))
+		return false;
+	else
+		return IsBalanced(tree->left) && IsBalanced(tree->right);
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"tree"};
-  return GenericTestMain(args, "is_tree_balanced.cc", "is_tree_balanced.tsv",
-                         &IsBalanced, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "tree" };
+	return GenericTestMain(args, "is_tree_balanced.cc", "is_tree_balanced.tsv",
+		&IsBalanced, DefaultComparator{}, param_names);
 }
