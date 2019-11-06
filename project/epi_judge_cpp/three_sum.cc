7c65478f56ec4c535e0ee235d6ca3df8891de74f
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-06 09:50:42 -0500

    First pass solution at 17.4.

--- project/epi_judge_cpp/three_sum.cc
+++ project/epi_judge_cpp/three_sum.cc
@@ -1,15 +1,26 @@
 #include <vector>
+#include <unordered_set>
 #include "test_framework/generic_test.h"
 using std::vector;
+using std::unordered_set;
 
 bool HasThreeSum(vector<int> A, int t) {
-  // TODO - you fill in here.
-  return true;
+	unordered_set<int> nums(A.begin(), A.end());
+	int count = 0;
+
+	for (auto i = nums.begin(); i != nums.end(); ++i) {
+		for (auto j = i; j != nums.end(); ++j) {
+			if (nums.find(t - (*i + *j)) != nums.end())
+				++count;
+		}
+	}
+
+	return count;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"A", "t"};
-  return GenericTestMain(args, "three_sum.cc", "three_sum.tsv", &HasThreeSum,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "A", "t" };
+	return GenericTestMain(args, "three_sum.cc", "three_sum.tsv", &HasThreeSum,
+		DefaultComparator{}, param_names);
 }
