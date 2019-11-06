b3277e89da637f2efa8da4221812ddf20d515f5b
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-06 09:31:45 -0500

    Solved 16.1.

--- project/epi_judge_cpp/number_of_score_combinations.cc
+++ project/epi_judge_cpp/number_of_score_combinations.cc
@@ -3,16 +3,30 @@
 using std::vector;
 
 int NumCombinationsForFinalScore(int final_score,
-                                 const vector<int>& individual_play_scores) {
-  // TODO - you fill in here.
-  return 0;
+	const vector<int>& individual_play_scores)
+{
+	vector<vector<int>> combos_for_score(individual_play_scores.size(), vector<int>(final_score + 1, 0));
+	for (auto& combo : combos_for_score)
+		combo[0] = 1;
+
+	for (int play = 0; play < combos_for_score.size(); ++play) {
+		for (int score = 1; score <= final_score; ++score) {
+			int without_play = (play > 0) ? combos_for_score[play - 1][score] : 0;
+			int with_play = (score >= individual_play_scores[play])
+				? combos_for_score[play][score - individual_play_scores[play]]
+				: 0;
+			combos_for_score[play][score] = without_play + with_play;
+		}
+	}
+
+	return combos_for_score.back()[final_score];
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"final_score", "individual_play_scores"};
-  return GenericTestMain(args, "number_of_score_combinations.cc",
-                         "number_of_score_combinations.tsv",
-                         &NumCombinationsForFinalScore, DefaultComparator{},
-                         param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "final_score", "individual_play_scores" };
+	return GenericTestMain(args, "number_of_score_combinations.cc",
+		"number_of_score_combinations.tsv",
+		&NumCombinationsForFinalScore, DefaultComparator{},
+		param_names);
 }
