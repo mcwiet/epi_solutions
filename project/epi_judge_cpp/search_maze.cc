c7ac379ff27a2dfca997633d733c0c84c2d16918
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-06 10:59:23 -0500

    Adding solution to 18.1

--- project/epi_judge_cpp/search_maze.cc
+++ project/epi_judge_cpp/search_maze.cc
@@ -1,87 +1,123 @@
 #include <istream>
 #include <string>
 #include <vector>
+#include <stack>
 #include "test_framework/generic_test.h"
 #include "test_framework/serialization_traits.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
 using std::vector;
+using std::stack;
 typedef enum { kWhite, kBlack } Color;
 struct Coordinate {
-  bool operator==(const Coordinate& that) const {
-    return x == that.x && y == that.y;
-  }
+	bool operator==(const Coordinate& that) const {
+		return x == that.x && y == that.y;
+	}
 
-  int x, y;
+	int x, y;
 };
-vector<Coordinate> SearchMaze(vector<vector<Color>> maze, const Coordinate& s,
-                              const Coordinate& e) {
-  // TODO - you fill in here.
-  return {};
+
+vector<Coordinate> GetValidUnvisitedNeighbors(const vector<vector<Color>>& maze, const Coordinate& c) {
+	vector<Coordinate> ret;
+	if (c.x - 1 >= 0 && maze[c.x - 1][c.y] != kBlack)
+		ret.emplace_back(Coordinate{ c.x - 1, c.y });
+	if (c.x + 1 < maze.size() && maze[c.x + 1][c.y] != kBlack)
+		ret.emplace_back(Coordinate{ c.x + 1, c.y });
+	if (c.y - 1 >= 0 && maze[c.x][c.y - 1] != kBlack)
+		ret.emplace_back(Coordinate{ c.x, c.y - 1 });
+	if (c.y + 1 < maze[c.x].size() && maze[c.x][c.y + 1] != kBlack)
+		ret.emplace_back(Coordinate{ c.x, c.y + 1 });
+
+	return ret;
+}
+
+bool SearchMazeHelper(vector<vector<Color>>& maze, vector<Coordinate>& path, const Coordinate& c, const Coordinate& e) {
+	path.emplace_back(c);
+
+	if (c == e)
+		return true;
+
+	maze[c.x][c.y] = kBlack;
+	auto neighbors = GetValidUnvisitedNeighbors(maze, c);
+	for (auto& neighbor : neighbors) {
+		if (SearchMazeHelper(maze, path, neighbor, e))
+			return true;
+	}
+
+	path.pop_back();
+	return false;
+}
+
+vector<Coordinate> SearchMaze(vector<vector<Color>> maze, const Coordinate& s, const Coordinate& e)
+{
+	vector<Coordinate> path;
+	SearchMazeHelper(maze, path, s, e);
+
+	return path;
 }
 template <>
 struct SerializationTraits<Color> : SerializationTraits<int> {
-  using serialization_type = Color;
+	using serialization_type = Color;
 
-  static serialization_type Parse(const std::string& str) {
-    return static_cast<serialization_type>(
-        SerializationTraits<int>::Parse(str));
-  }
+	static serialization_type Parse(const std::string& str) {
+		return static_cast<serialization_type>(
+			SerializationTraits<int>::Parse(str));
+	}
 
-  static serialization_type JsonParse(const json_parser::Json& json_object) {
-    return static_cast<serialization_type>(
-        SerializationTraits<int>::JsonParse(json_object));
-  }
+	static serialization_type JsonParse(const json_parser::Json& json_object) {
+		return static_cast<serialization_type>(
+			SerializationTraits<int>::JsonParse(json_object));
+	}
 };
 
 template <>
 struct SerializationTraits<Coordinate> : UserSerTraits<Coordinate, int, int> {
-  static std::vector<std::string> GetMetricNames(const std::string& arg_name) {
-    return {};
-  }
+	static std::vector<std::string> GetMetricNames(const std::string& arg_name) {
+		return {};
+	}
 
-  static std::vector<int> GetMetrics(const Coordinate& x) { return {}; }
+	static std::vector<int> GetMetrics(const Coordinate& x) { return {}; }
 };
 
 bool PathElementIsFeasible(const vector<vector<Color>>& maze,
-                           const Coordinate& prev, const Coordinate& cur) {
-  if (!(0 <= cur.x && cur.x < maze.size() && 0 <= cur.y &&
-        cur.y < maze[cur.x].size() && maze[cur.x][cur.y] == kWhite)) {
-    return false;
-  }
-  return cur == Coordinate{prev.x + 1, prev.y} ||
-         cur == Coordinate{prev.x - 1, prev.y} ||
-         cur == Coordinate{prev.x, prev.y + 1} ||
-         cur == Coordinate{prev.x, prev.y - 1};
+	const Coordinate& prev, const Coordinate& cur) {
+	if (!(0 <= cur.x && cur.x < maze.size() && 0 <= cur.y &&
+		cur.y < maze[cur.x].size() && maze[cur.x][cur.y] == kWhite)) {
+		return false;
+	}
+	return cur == Coordinate{ prev.x + 1, prev.y } ||
+		cur == Coordinate{ prev.x - 1, prev.y } ||
+		cur == Coordinate{ prev.x, prev.y + 1 } ||
+		cur == Coordinate{ prev.x, prev.y - 1 };
 }
 
 bool SearchMazeWrapper(TimedExecutor& executor,
-                       const vector<vector<Color>>& maze, const Coordinate& s,
-                       const Coordinate& e) {
-  vector<vector<Color>> copy = maze;
+	const vector<vector<Color>>& maze, const Coordinate& s,
+	const Coordinate& e) {
+	vector<vector<Color>> copy = maze;
 
-  auto path = executor.Run([&] { return SearchMaze(copy, s, e); });
+	auto path = executor.Run([&] { return SearchMaze(copy, s, e); });
 
-  if (path.empty()) {
-    return s == e;
-  }
+	if (path.empty()) {
+		return s == e;
+	}
 
-  if (!(path.front() == s) || !(path.back() == e)) {
-    throw TestFailure("Path doesn't lay between start and end points");
-  }
+	if (!(path.front() == s) || !(path.back() == e)) {
+		throw TestFailure("Path doesn't lay between start and end points");
+	}
 
-  for (size_t i = 1; i < path.size(); i++) {
-    if (!PathElementIsFeasible(maze, path[i - 1], path[i])) {
-      throw TestFailure("Path contains invalid segments");
-    }
-  }
+	for (size_t i = 1; i < path.size(); i++) {
+		if (!PathElementIsFeasible(maze, path[i - 1], path[i])) {
+			throw TestFailure("Path contains invalid segments");
+		}
+	}
 
-  return true;
+	return true;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "maze", "s", "e"};
-  return GenericTestMain(args, "search_maze.cc", "search_maze.tsv",
-                         &SearchMazeWrapper, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "executor", "maze", "s", "e" };
+	return GenericTestMain(args, "search_maze.cc", "search_maze.tsv",
+		&SearchMazeWrapper, DefaultComparator{}, param_names);
 }
