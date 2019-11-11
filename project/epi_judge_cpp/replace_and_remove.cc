05c4f22da0334396cfb81979d1896273606bdc7e
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-11 11:45:58 -0500

    Adding solution for 6.4

--- project/epi_judge_cpp/replace_and_remove.cc
+++ project/epi_judge_cpp/replace_and_remove.cc
@@ -7,32 +7,54 @@ using std::string;
 using std::vector;
 
 int ReplaceAndRemove(int size, char s[]) {
-  // TODO - you fill in here.
-  return 0;
+	int num_a = 0;
+	int write = 0;
+	for (int i = 0; i < size; ++i) {
+		if (s[i] != 'b')
+			s[write++] = s[i];
+		if (s[i] == 'a')
+			++num_a;
+	}
+
+	int current = write - 1;
+	write = write + num_a - 1;
+	int final_index = write + 1;
+	while (current >= 0) {
+		if (s[current] == 'a') {
+			s[write--] = 'd';
+			s[write--] = 'd';
+		}
+		else {
+			s[write--] = s[current];
+		}
+		--current;
+	}
+
+	return final_index;
 }
 vector<string> ReplaceAndRemoveWrapper(TimedExecutor& executor, int size,
-                                       const vector<string>& s) {
-  std::vector<char> s_copy(s.size(), '\0');
-  for (int i = 0; i < s.size(); ++i) {
-    if (!s[i].empty()) {
-      s_copy[i] = s[i][0];
-    }
-  }
+	const vector<string>& s) {
+	std::vector<char> s_copy(s.size(), '\0');
+	for (int i = 0; i < s.size(); ++i) {
+		if (!s[i].empty()) {
+			s_copy[i] = s[i][0];
+		}
+	}
 
-  int res_size =
-      executor.Run([&] { return ReplaceAndRemove(size, s_copy.data()); });
+	int res_size =
+		executor.Run([&] { return ReplaceAndRemove(size, s_copy.data()); });
 
-  vector<string> result;
-  for (int i = 0; i < res_size; ++i) {
-    result.emplace_back(string(1, s_copy[i]));
-  }
-  return result;
+	vector<string> result;
+	for (int i = 0; i < res_size; ++i) {
+		result.emplace_back(string(1, s_copy[i]));
+	}
+	return result;
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "size", "s"};
-  return GenericTestMain(args, "replace_and_remove.cc",
-                         "replace_and_remove.tsv", &ReplaceAndRemoveWrapper,
-                         DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "executor", "size", "s" };
+	return GenericTestMain(args, "replace_and_remove.cc",
+		"replace_and_remove.tsv", &ReplaceAndRemoveWrapper,
+		DefaultComparator{}, param_names);
 }
