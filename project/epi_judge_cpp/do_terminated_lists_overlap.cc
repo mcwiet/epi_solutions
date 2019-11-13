7c7c44624e834147ac5dbacec4eecc3a7904b046
Mike Cwietniewicz <mike.cwiet@gmail.com>
2019-11-13 08:54:10 -0500

    Adding solution for 7.4. Improving in next commit.

--- project/epi_judge_cpp/do_terminated_lists_overlap.cc
+++ project/epi_judge_cpp/do_terminated_lists_overlap.cc
@@ -1,52 +1,65 @@
 #include <memory>
+#include <unordered_set>
 #include "list_node.h"
 #include "test_framework/generic_test.h"
 #include "test_framework/test_failure.h"
 #include "test_framework/timed_executor.h"
 using std::shared_ptr;
+using std::unordered_set;
 
 shared_ptr<ListNode<int>> OverlappingNoCycleLists(
-    shared_ptr<ListNode<int>> l0, shared_ptr<ListNode<int>> l1) {
-  // TODO - you fill in here.
-  return nullptr;
+	shared_ptr<ListNode<int>> l0, shared_ptr<ListNode<int>> l1) {
+	unordered_set<shared_ptr<ListNode<int>>> l0_nodes;
+
+	while (l0 != nullptr) {
+		l0_nodes.insert(l0);
+		l0 = l0->next;
+	}
+
+	while (l0_nodes.find(l1) == l0_nodes.end() && l1 != nullptr)
+		l1 = l1->next;
+
+	return l1;
 }
 void OverlappingNoCycleListsWrapper(TimedExecutor& executor,
-                                    shared_ptr<ListNode<int>> l0,
-                                    shared_ptr<ListNode<int>> l1,
-                                    shared_ptr<ListNode<int>> common) {
-  if (common) {
-    if (l0) {
-      auto i = l0;
-      while (i->next) {
-        i = i->next;
-      }
-      i->next = common;
-    } else {
-      l0 = common;
-    }
+	shared_ptr<ListNode<int>> l0,
+	shared_ptr<ListNode<int>> l1,
+	shared_ptr<ListNode<int>> common) {
+	if (common) {
+		if (l0) {
+			auto i = l0;
+			while (i->next) {
+				i = i->next;
+			}
+			i->next = common;
+		}
+		else {
+			l0 = common;
+		}
 
-    if (l1) {
-      auto i = l1;
-      while (i->next) {
-        i = i->next;
-      }
-      i->next = common;
-    } else {
-      l1 = common;
-    }
-  }
+		if (l1) {
+			auto i = l1;
+			while (i->next) {
+				i = i->next;
+			}
+			i->next = common;
+		}
+		else {
+			l1 = common;
+		}
+	}
 
-  auto result = executor.Run([&] { return OverlappingNoCycleLists(l0, l1); });
+	auto result = executor.Run([&] { return OverlappingNoCycleLists(l0, l1); });
 
-  if (result != common) {
-    throw TestFailure("Invalid result");
-  }
+	if (result != common) {
+		throw TestFailure("Invalid result");
+	}
 }
 
 int main(int argc, char* argv[]) {
-  std::vector<std::string> args{argv + 1, argv + argc};
-  std::vector<std::string> param_names{"executor", "l0", "l1", "common"};
-  return GenericTestMain(
-      args, "do_terminated_lists_overlap.cc", "do_terminated_lists_overlap.tsv",
-      &OverlappingNoCycleListsWrapper, DefaultComparator{}, param_names);
+	std::vector<std::string> args{ argv + 1, argv + argc };
+	std::vector<std::string> param_names{ "executor", "l0", "l1", "common" };
+	return GenericTestMain(
+		args, "do_terminated_lists_overlap.cc", "do_terminated_lists_overlap.tsv",
+		&OverlappingNoCycleListsWrapper, DefaultComparator{}, param_names);
 }
